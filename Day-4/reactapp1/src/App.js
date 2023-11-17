c1e92ff84437873ce3e0e02861b65e6a410fb03f
Geethanjali-Selvarajan <150216904+Geethanjali-Selvarajan@users.noreply.github.com>
2023-11-17 09:59:10 +0530

    Day-4

---
+++ Day-4/reactapp1/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Listitems from './components/List/Listitems';
+import Table from './components/List/Table';
+function App() {
+  return (
+    <div className="App">
+     <Listitems/> 
+     <Table/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Day-4/reactapp2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ButtonUsage from './components/ButtonUsage';
+import Form from './components/Form';
+function App() {
+  return (
+    <div className="App">
+    <ButtonUsage/>
+     <Form/>
+    </div>
+  );
+}
+
+export default App;
